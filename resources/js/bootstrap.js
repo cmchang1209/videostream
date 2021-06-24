fc94fea74d6fd6660a2f53cdfaaa6278996734f4
cmchang <cmchang120956@gmail.com>
2021-06-24 01:03:34 +0000

    update Tournament

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -20,14 +20,14 @@ window.Base64 = require('js-base64').Base64
 
 import ElementUI from 'element-ui'
 // Fixes an issue with filters not working on mobile
-ElementUi.Select.computed.readonly = function() {
+ElementUI.Select.computed.readonly = function() {
     // trade-off for IE input readonly problem: https://github.com/ElemeFE/element/issues/10403
     const isIE = !this.$isServer && !Number.isNaN(Number(document.documentMode));
 
     return !(this.filterable || this.multiple || !isIE) && !this.visible
 }
 
-export default ElementUi
+export default ElementUI
 
 import 'element-ui/lib/theme-chalk/display.css'
 Vue.use(ElementUI)
